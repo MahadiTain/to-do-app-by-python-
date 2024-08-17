3e01657a1d2b52b8b70f9592c7c39387e529017a
MahadiTain <mahaditain14@gmail.com>
2024-08-18 00:12:45 +0600

    trying to make a big

---
+++ student grade management system.py
@@ -0,0 +1,55 @@
+#initializing dictionary
+student_grades = { }
+#add a new student 
+def add_student(name, grade):
+    student_grades[name] = grade
+    print(f"Added {name} with a {grade}")
+#update student
+def update_student (name, grade):
+    if name in student_grades:
+        student_grades[name] = grade
+        print(f"{name} with marks are updated {grade}")
+    else:
+        print(f"{name} is not found here!")
+# delete student 
+def delete_student(name):
+    if name in student_grades:
+        del student_grades[name]
+        print(f"{name} has been successfully deleted")
+    else:
+        print(f"{name} is not found!")
+def display_all_students():
+    if student_grades:
+        for name, grade in student_grades.items():
+            print(f"{name} : {grade}")
+           # print(f"No students found/added")
+    else:
+        print("No students found/added")
+def main():
+    while True:
+        print('\n Student Grades Management System')
+        print("1. Add Student")
+        print("2. Update Student")
+        print("3. Delete Student")
+        print("4. View Student")
+        print("5. Exit")
+        choice = int(input("Enter your choice = "))
+        if choice ==1:
+            name = input("Enter student name =")
+            grade = float(input("Enter student grade ="))
+            add_student(name, grade)
+        elif choice ==2:
+            name = input ("Enter student name =")
+            grade = float(input("Enter student grade ="))
+            update_student(name, grade)
+        elif choice ==3:
+            name = input("Enter student name ")
+            delete_student(name)
+        elif choice ==4:
+            display_all_students()
+        elif choice ==5:
+            print("Closing the program")
+            break
+        else:
+            print("Invalid choice")
+main()    
